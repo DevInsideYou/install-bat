a7a00adca9384d5ecfc8b02c680386fea1688b69
Vladyslav Pekker <agilesteel@gmail.com>
2020-06-09 15:09:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Installs [bat 0.15.4](https://github.com/sharkdp/bat) on [Ubuntu](https://www.ubuntu.com/)
+
+```bash
+wget -q https://script.install.devinsideyou.com/bat
+sudo chmod +x bat && ./bat
+```
+
+# Specifies [version](https://github.com/sharkdp/bat/releases) (optional)
+
+```bash
+wget -q https://script.install.devinsideyou.com/bat
+sudo chmod +x bat && ./bat 0.15.4
+```
---
+++ install-bat.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# remove yourself
+rm $0
+
+FALLBACK_VERSION="0.15.4"
+
+if [ "$1" != "" ]; then
+    VERSION="$1"
+else
+    VERSION=$FALLBACK_VERSION
+fi
+
+DEBIAN_PACKAGE=bat_${VERSION}_amd64.deb
+
+# install bat
+wget https://github.com/sharkdp/bat/releases/download/v${VERSION}/$DEBIAN_PACKAGE
+sudo dpkg -i $DEBIAN_PACKAGE
+rm $DEBIAN_PACKAGE
+
+echo
+echo '"bat" is now on the path'
